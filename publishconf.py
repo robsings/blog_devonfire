8906b3eeee7808acf49c8b791b0d51438b79c10d
robsings <saintsrobert25@gmail.com>
2025-04-21 14:25:17 -0300

    Atualiza .gitignore e remove arquivos desnecessários do versionamento

--- publishconf.py
+++ publishconf.py
@@ -15,6 +15,9 @@ RELATIVE_URLS = False
 FEED_ALL_ATOM = "feeds/all.atom.xml"
 CATEGORY_FEED_ATOM = "feeds/{slug}.atom.xml"
 
+EXTRA_PATH_METADATA = {
+    './content/extra/robots.txt': {'path': 'robots.txt'},
+}
 
 DELETE_OUTPUT_DIRECTORY = True
 
